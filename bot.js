3b1aa0a1cc6bf03467b3a662f3fa4b7b077ae286
mainalienbot <110978638+mainalienbot@users.noreply.github.com>
2022-08-12 12:11:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,21 +33,21 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
+global.owner = ['+2348100236360',''] //owner number +2348100236360
+global.wame = ['+2348100236360'] 
+global.pemilik = ['+2348100236360'] //owner number +2348100236360
+global.premium = ['+2348100236360'] //premium number +2348100236360
 global.pengguna = 'Drips' //username
 global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.botnma = 'ZIM BOT INC' //Alien
+global.botname = 'Alien Bot'
+global.ownernma = 'Bashir' //Bashir
+global.packname = 'Bashir' //sticker package name
+global.author = 'By Bashir Memes' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
-global.dripstyping = false
+global.dripstyping = true
 global.dripsreadgroup = false
 global.dripsreadall = false
 global.dripsrecord = false
